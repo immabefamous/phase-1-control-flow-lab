d3e26b5cc412958dbd87762d8fd871d35d77187f
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-15 16:53:31 -0400

    commit tree 0beeef01fc07572b20a5ca3facff374c3e1e123a

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+  let result;
+  if (x <= 400){ 
+    result = 'This one is on me!'
+  }
+    else if ( x > 2000 && x < 2500){
+      result = 'I will gladly take your thirty bucks.'
+    }
+    else if (x >= 2500){
+      result = 'No can do.'
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
   // Write your code here!
+  let re;
+  if (x === 'NYC') {
+    re = "Ok, sounds good."
+  }
+  else if (x !== 'NYC') {
+    re = "No go."
+  }
+  return re
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
   // Write your code here!
+
+  let option;
+  switch (x) {
+    case 'generous':
+     option = "Thank you so much.";
+     break;
+  
+    case 'not as generous':
+      option = "Thank you."
+      break;
+
+    case 'thanks for everything':
+      option = "Bye."
+      break;
+    
+    default:
+      option = "Unknown"
+      break;
+  }
+  return option
 }
